2b018aff283356596d0025241d469419f099af31
Gio Perrotta <gioperrotta@gmail.com>
2023-06-14 20:48:13 -0300

    estudos iniciais

---
+++ index copy.js
@@ -0,0 +1,13 @@
+function clickMenu() {
+   if (menuMobile.style.display == "none") {
+      menuMobile.style.display = "flex";
+   } else {
+      menuMobile.style.display = "none";
+   }
+
+   if (menuCelular.style.display == "none") {
+      menuCelular.style.display = "flex";
+   } else {
+      menuCelular.style.display = "none";
+   }
+}
